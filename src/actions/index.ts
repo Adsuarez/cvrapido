b3a639105b52c463789b8ad81369f37bc12f56e9
Andres <adsuarezq@gmail.com>
2025-10-28 15:13:13 -0400

    import and call function inside action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,11 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { pdfReader } from "../files";
 
 export const server = {
   getConversion: defineAction({
     handler: async () => {
+      pdfReader();
       return "hola";
     },
   }),
