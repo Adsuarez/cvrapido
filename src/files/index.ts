30fa7ddd692beba50a2af8c185e87f09ee10b261
Andres <adsuarezq@gmail.com>
2025-10-29 16:14:20 -0400

    modify function to save and return all pdf information on sortedRows variable

--- src/files/index.ts
+++ src/files/index.ts
@@ -1,23 +1,30 @@
 import { PdfReader } from "pdfreader";
 
-let rows = {}; // indexed by y-position
+type Row = {
+  [y: string]: string[];
+};
+let rows: Row = {}; // indexed by y-position
+let sortedRows: string[] = [];
 
 function printRows() {
   Object.keys(rows) // => array of y-positions (type: float)
     .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
-    .forEach((y) => console.log((rows[y] || []).join("")));
+    .forEach((y) => {
+      sortedRows.push((rows[y] || []).join(""));
+    });
 }
 
-export function pdfReader() {
+export async function readPDF() {
   new PdfReader().parseFileItems("./example.pdf", function (err, item) {
     if (!item || item.page) {
       // end of file, or page
       printRows();
-      console.log("PAGE:", item?.page);
+      //console.log("PAGE:", item?.page);
       rows = {}; // clear rows for next page
     } else if (item.text) {
       // accumulate text items into rows object, per line
       (rows[item.y] = rows[item.y] || []).push(item.text);
     }
   });
+  return sortedRows;
 }
