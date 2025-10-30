5e45a1b3d0139f1fbece8bae2d8aeb67ed40dafe
Andres <adsuarezq@gmail.com>
2025-10-30 16:48:08 -0400

    import LinkedInResume class and use it to get contactItems.

--- src/files/utils/get-contact.ts
+++ src/files/utils/get-contact.ts
@@ -1,4 +1,4 @@
-import { Contact, Skill } from "@/files/classes";
+import { Contact, LinkedInResume, Skill } from "@/files/classes";
 import { Languages } from "@/files/consts";
 
 export async function getContact({
@@ -10,11 +10,9 @@ export async function getContact({
 }) {
   const contact = new Contact({ language });
   const skill = new Skill({ language });
-  console.log("\n\n----------", contact.text, "-----------\n\n");
-  const contactIndex = pdfParsed.search(contact.regexp);
-  const skillIndex = pdfParsed.search(skill.regexp);
+  const linkedinResume = new LinkedInResume({ contact, skill, pdfParsed });
 
-  if (contactIndex >= 0) {
-    console.log({ contactIndex, skillIndex });
-  }
+  console.log("\n\n----------", contact.text, "-----------\n\n");
+  const contactItems = linkedinResume.getContact;
+  console.log({ contactItems });
 }
