e4c531b6613306645e4f6f73b37c3c6d7e9c8a73
Andres <adsuarezq@gmail.com>
2025-10-30 15:35:23 -0400

    import and use skill instance from class

--- src/files/utils/get-contact.ts
+++ src/files/utils/get-contact.ts
@@ -1,4 +1,4 @@
-import { Contact } from "@/files/classes";
+import { Contact, Skill } from "@/files/classes";
 import { Languages } from "@/files/consts";
 
 export async function getContact({
@@ -9,9 +9,12 @@ export async function getContact({
   language: Languages;
 }) {
   const contact = new Contact({ language });
+  const skill = new Skill({ language });
   console.log("\n\n----------", contact.text, "-----------\n\n");
   const contactIndex = pdfParsed.search(contact.regexp);
+  const skillIndex = pdfParsed.search(skill.regexp);
+
   if (contactIndex >= 0) {
-    console.log({ contactIndex });
+    console.log({ contactIndex, skillIndex });
   }
 }
