59c79937a430a4047ba10a6d33cfcbaf8ce6a5af
Andres <adsuarezq@gmail.com>
2025-10-30 16:47:39 -0400

    replace english with spanish because example3 pdf is a spanish resume

--- src/files/pdf-parse.ts
+++ src/files/pdf-parse.ts
@@ -3,11 +3,11 @@ import { linkedinConversion } from "@/files/linkedin-conversion";
 import { Languages } from "@/files/consts";
 
 export async function parsePdf() {
-  const parser = new PDFParse({ url: "./example2.pdf" });
+  const parser = new PDFParse({ url: "./example3.pdf" });
   const pdfParsed = await parser.getText();
   const result = await linkedinConversion({
     pdfParsed: pdfParsed.text,
-    language: Languages.English,
+    language: Languages.Spanish,
   });
   return result;
 }
