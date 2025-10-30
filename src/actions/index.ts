08928ca586db7e2e40d958e37338942baf83b975
Andres <adsuarezq@gmail.com>
2025-10-30 11:32:55 -0400

    remove unused imports and functions replaced with parsePdf

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,26 +1,8 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
-import { readLinealPdf } from "../files/lineal-pdf";
-import { readColumnsPdf } from "../files/columns-pdf";
 import { parsePdf } from "../files/pdf-parse";
 
-let result: string[] = [];
 export const server = {
-  getLinealPdfConversion: defineAction({
-    handler: async () => {
-      await readLinealPdf().then((res) => {
-        result = res;
-      });
-
-      return result;
-    },
-  }),
-  getColumnsPdfConversion: defineAction({
-    handler: async () => {
-      let result = await readColumnsPdf({ columns: 2 });
-      return result;
-    },
-  }),
   parsePdf: defineAction({
     handler: async () => {
       return await parsePdf();
