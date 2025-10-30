f15590663b3c01a0d5448e510815b306358d333d
Andres <adsuarezq@gmail.com>
2025-10-30 12:31:33 -0400

    send Language.english in linkedinConversion functtion called

--- src/files/pdf-parse.ts
+++ src/files/pdf-parse.ts
@@ -1,9 +1,13 @@
 import { PDFParse } from "pdf-parse";
 import { linkedinConversion } from "./linkedin-conversion";
+import { Languages } from "./consts";
 
 export async function parsePdf() {
   const parser = new PDFParse({ url: "./example2.pdf" });
   const pdfParsed = await parser.getText();
-  const result = await linkedinConversion({ pdfParsed });
+  const result = await linkedinConversion({
+    pdfParsed: pdfParsed.text,
+    language: Languages.English,
+  });
   return result;
 }
