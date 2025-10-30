f52bb22242ff89c688025e6f49dfbfd31b3da957
Andres <adsuarezq@gmail.com>
2025-10-30 12:30:35 -0400

    add Languages enum in function params and send inside getContact

--- src/files/linkedin-conversion.ts
+++ src/files/linkedin-conversion.ts
@@ -1,9 +1,13 @@
-import type { TextResult } from "pdf-parse";
+import type { Languages } from "./consts";
+import { getContact } from "./utils/get-contact";
 
 export async function linkedinConversion({
   pdfParsed,
+  language,
 }: {
-  pdfParsed: TextResult;
+  pdfParsed: string;
+  language: Languages;
 }) {
-  console.log(pdfParsed.text);
+  console.log(pdfParsed);
+  await getContact({ pdfParsed, language });
 }
