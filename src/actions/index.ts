28ff8d09a5f114c175b90bade3fcd415b6ae22cd
Andres <adsuarezq@gmail.com>
2025-10-31 11:41:29 -0400

    import and add language and give it to parsePdf function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,16 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 import { parsePdf } from "@/files/pdf-parse";
+import { Languages } from "@/files/consts";
 
 export const server = {
   parsePdf: defineAction({
     input: z.object({
       url: z.string(),
+      language: z.nativeEnum(Languages),
     }),
     handler: async (input) => {
-      return await parsePdf({ url: input.url });
+      return await parsePdf({ url: input.url, language: input.language });
     },
   }),
 };
