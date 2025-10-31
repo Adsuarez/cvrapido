2e9f59a9676d5ea7410556327eabfdf37cfe0e8a
Andres <adsuarezq@gmail.com>
2025-10-31 11:41:06 -0400

    modify function to get language as a property

--- src/files/pdf-parse.ts
+++ src/files/pdf-parse.ts
@@ -2,12 +2,18 @@ import { PDFParse } from "pdf-parse";
 import { linkedinConversion } from "@/files/linkedin-conversion";
 import { Languages } from "@/files/consts";
 
-export async function parsePdf({ url }: { url: string }) {
+export async function parsePdf({
+  url,
+  language,
+}: {
+  url: string;
+  language: Languages;
+}) {
   const parser = new PDFParse({ url });
   const pdfParsed = await parser.getText();
   const result = await linkedinConversion({
     pdfParsed: pdfParsed.text,
-    language: Languages.Spanish,
+    language,
   });
   return result;
 }
