9a08bed738e0b1ec2320a40842cff1ddb677f37f
Andres <adsuarezq@gmail.com>
2025-10-31 09:45:09 -0400

    reorder code lines to better understanding of code

--- src/files/utils/extractor.ts
+++ src/files/utils/extractor.ts
@@ -14,8 +14,9 @@ export function extractor({
 }) {
   const contactIndex = pdfParsed.search(contact.regexp);
   const skillIndex = pdfParsed.search(skill.regexp);
-  const mobileIndex = pdfParsed.search(MOBILE_WORD[language]);
   const allContact = pdfParsed.slice(contactIndex, skillIndex);
+
+  const mobileIndex = pdfParsed.search(MOBILE_WORD[language]);
   const mobile = allContact
     .slice(contactIndex + contact.length, mobileIndex)
     .replace("(", "")
