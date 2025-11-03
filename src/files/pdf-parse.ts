d715d516d0b16f8431111b800eac08624e76ef67
Andres <adsuarezq@gmail.com>
2025-11-03 11:47:17 -0500

    remove language as a parameter

--- src/files/pdf-parse.ts
+++ src/files/pdf-parse.ts
@@ -1,19 +1,11 @@
 import { PDFParse } from "pdf-parse";
 import { linkedinConversion } from "@/files/linkedin-conversion";
-import { Languages } from "@/files/consts";
 
-export async function parsePdf({
-  url,
-  language,
-}: {
-  url: string;
-  language: Languages;
-}) {
+export async function parsePdf({ url }: { url: string }) {
   const parser = new PDFParse({ url });
   const pdfParsed = await parser.getText();
   const result = await linkedinConversion({
     pdfParsed: pdfParsed.text,
-    language,
   });
   return result;
 }
