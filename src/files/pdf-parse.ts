9b0e8d7827fd54645c58ab8c856d0246cc2e81ec
Andres <adsuarezq@gmail.com>
2025-11-03 14:36:26 -0500

    replace result with object type conversionResponse and return from function

--- src/files/pdf-parse.ts
+++ src/files/pdf-parse.ts
@@ -1,11 +1,12 @@
 import { PDFParse } from "pdf-parse";
 import { linkedinConversion } from "@/files/linkedin-conversion";
+import type { ConversionResponse } from "./consts";
 
 export async function parsePdf({ url }: { url: string }) {
   const parser = new PDFParse({ url });
   const pdfParsed = await parser.getText();
-  const result = await linkedinConversion({
+  const { data, error }: ConversionResponse = await linkedinConversion({
     pdfParsed: pdfParsed.text,
   });
-  return result;
+  return { data, error };
 }
