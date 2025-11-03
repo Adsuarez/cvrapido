52ec44894ddad0ead897b34945185d0253bccac5
Andres <adsuarezq@gmail.com>
2025-11-03 11:47:54 -0500

    remove import and languages from input and handler

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,17 +1,15 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 import { parsePdf } from "@/files/pdf-parse";
-import { Languages } from "@/files/consts";
 
 export const server = {
   parsePdf: defineAction({
     input: z.object({
       url: z.string(),
-      language: z.nativeEnum(Languages),
     }),
     handler: async (input) => {
-      const { url, language } = input;
-      return await parsePdf({ url, language });
+      const { url } = input;
+      return await parsePdf({ url });
     },
   }),
 };
