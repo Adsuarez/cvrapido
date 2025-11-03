a1c4d478e539f27c667e8cb96fd30307bc646542
Andres <adsuarezq@gmail.com>
2025-11-03 14:37:49 -0500

    add logic to store values in data and error to give a conversion response object

--- src/files/linkedin-conversion.ts
+++ src/files/linkedin-conversion.ts
@@ -1,10 +1,20 @@
 import { getContact } from "@/files/utils/get-contact";
 import { getLanguage } from "./utils/get-language";
+import type {
+  ConversionResponse,
+  ConversionResponseData,
+  ConversionResponseError,
+} from "./consts";
 
 export async function linkedinConversion({ pdfParsed }: { pdfParsed: string }) {
   console.log(pdfParsed);
   const language = await getLanguage({ pdfParsed });
-  if (language) {
-    await getContact({ pdfParsed, language });
-  }
+  let data: ConversionResponseData = {};
+  let error: ConversionResponseError =
+    "The document provided is not compatible with a LinkedIn resume PDF";
+  if (!language) return { data, error };
+  const { contactItems } = await getContact({ pdfParsed, language });
+  error = null;
+  data = { contactItems };
+  return { data, error };
 }
