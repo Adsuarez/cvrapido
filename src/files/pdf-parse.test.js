aad72106a7b44b195252e3239203427075b9ad7a
Andres <adsuarezq@gmail.com>
2025-11-04 16:45:31 -0500

    move "with" word from describe statement to first test to organize better the sintaxis. Add second test for pdf without mobile number

--- src/files/pdf-parse.test.js
+++ src/files/pdf-parse.test.js
@@ -1,14 +1,21 @@
 import { expect, test, describe } from 'vitest'
 import {parsePdf} from "@/files/pdf-parse.ts"
 
-describe("Reading linkedin resume with", ()=>{
-  test("Mobile number and email should return mobile and email strings with length greater than cero", async ()=>{
+describe("Reading linkedin resume", ()=>{
+  test("with Mobile number and email should return mobile and email strings with length greater than cero", async ()=>{
     const {data, error} = await parsePdf({url: "./public/example2.pdf"})
     if(!error){
       const mobileLength = data?.contactItems?.mobile?.length
       const emailLength =  data?.contactItems?.email?.length
       expect(mobileLength).toBeGreaterThan(0)
       expect(emailLength).toBeGreaterThan(0)
+    }
+  });
+  test("without Mobile number should return a mobile strings with length equals to cero", async ()=>{
+    const {data, error} = await parsePdf({url: "./public/example.pdf"})
+    if(!error){
+      const mobileLength = data?.contactItems?.mobile?.length
+      expect(mobileLength).toBe(0);
     }
   })
 })
