2d2683a1e2a99f52307884a8960fbad547ed5ad4
Andres <adsuarezq@gmail.com>
2025-11-05 10:39:47 -0500

    replace deconstruction object with finalResponse const to simplify understanding of usage

--- src/files/pdf-parse.ts
+++ src/files/pdf-parse.ts
@@ -5,8 +5,8 @@ import type { ConversionResponse } from "@/files/consts.ts";
 export async function parsePdf({ url }: { url: string }) {
   const parser = new PDFParse({ url });
   const pdfParsed = await parser.getText();
-  const { data, error }: ConversionResponse = await linkedinConversion({
+  const finalResponse: ConversionResponse = await linkedinConversion({
     pdfParsed: pdfParsed.text,
   });
-  return { data, error };
+  return finalResponse;
 }
