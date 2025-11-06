b7d1bf13252da0760db32b1aec7cd243e1981d92
Andres <adsuarezq@gmail.com>
2025-11-06 16:50:38 -0500

    create directory contact and file contact-extractor to separate logic from extractor file in utils.

---
+++ src/files/contact/contact-extractor.ts
@@ -0,0 +1,37 @@
+import type { ContactWord, SkillWord } from "@/files/words/classes.ts";
+import { MOBILE_WORD } from "@/files/words/consts.ts";
+import { EMAIL_REGEXP, type Languages } from "@/files/consts.ts";
+import type { ContactItems } from "../classes";
+
+export function contactExtractor({
+  pdfParsed,
+  language,
+  contactWord,
+  skillIndex,
+}: {
+  pdfParsed: string;
+  language: Languages;
+  contactWord: ContactWord;
+  skillIndex: number;
+}) {
+  const contactIndex = pdfParsed.search(contactWord.regexp);
+  const allContact = pdfParsed.slice(contactIndex, skillIndex);
+
+  const mobileIndex = pdfParsed.search(MOBILE_WORD[language]);
+  const mobile =
+    mobileIndex >= 0
+      ? allContact
+          .slice(contactIndex + contactWord.length, mobileIndex)
+          .replace("(", "")
+          .trim()
+      : "";
+
+  const emailIndex = allContact.search(EMAIL_REGEXP);
+  const linkedinUrlIndex = allContact.search("www.linkedin");
+  const email = allContact
+    .slice(emailIndex, linkedinUrlIndex)
+    .replaceAll("\n", "");
+
+  const contactItems: ContactItems = { email, mobile };
+  return contactItems;
+}
