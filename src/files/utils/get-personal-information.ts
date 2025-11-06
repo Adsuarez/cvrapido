ce2692a9604471546f0d7ed878a90bdb7730e137
Andres <adsuarezq@gmail.com>
2025-11-06 14:26:52 -0500

    Add logic to check if last element is part of a profession to create correctly profesionChunks and join it.

--- src/files/utils/get-personal-information.ts
+++ src/files/utils/get-personal-information.ts
@@ -13,16 +13,48 @@ export function getPersonalInformation({
   const location = dirtyArray.pop() ?? "";
   let fullname = "";
   let profession = "";
+  const lastElementOnDirtyArray = dirtyArray[dirtyArray.length - 1];
   const isLastElementPartOfAProfession =
-    dirtyArray[dirtyArray.length - 1].includes("|");
+    lastElementOnDirtyArray.includes("|") ||
+    lastElementOnDirtyArray.includes(", ");
 
   if (isLastElementPartOfAProfession) {
     const reversedDirtyArray = dirtyArray.toReversed();
-    const professionChunks = reversedDirtyArray
-      .filter(
-        (element) => element.includes("|") || element.search(/[1-9]/g) >= 0
-      )
-      .toReversed();
+    const indexes: number[] = [];
+
+    reversedDirtyArray.forEach((el, index) => {
+      if (
+        el.includes("|") ||
+        el.search(/[1-9]/g) >= 0 ||
+        el.includes(" and ") ||
+        el.includes(", ")
+      ) {
+        indexes.push(index);
+      }
+    });
+
+    const cleanIndexes: number[] = [];
+
+    if (indexes.length > 0) {
+      for (let i = 0; i < indexes.length; i++) {
+        if (i === 0) {
+          cleanIndexes.push(indexes[i]);
+        } else {
+          const previousElement = indexes[i - 1];
+          const isThisElementOnlyOneValueMayorOfPreviousValue =
+            previousElement === indexes[i] - 1;
+          if (isThisElementOnlyOneValueMayorOfPreviousValue) {
+            cleanIndexes.push(indexes[i]);
+          } else {
+            break;
+          }
+        }
+      }
+    }
+
+    const professionChunks = cleanIndexes
+      .map((index) => reversedDirtyArray[index])
+      .reverse();
 
     const indexWhereProfessionStarts = dirtyArray.indexOf(professionChunks[0]);
     fullname = dirtyArray[indexWhereProfessionStarts - 1];
@@ -38,7 +70,6 @@ export function getPersonalInformation({
 
     if (indexes.length > 0) {
       profession = dirtyArray.slice(indexes[0], dirtyArray.length).join(" ");
-      console.log({ dirtyArray });
       fullname = dirtyArray[indexes[0] - 1];
     } else {
       profession = dirtyArray.pop() ?? "";
