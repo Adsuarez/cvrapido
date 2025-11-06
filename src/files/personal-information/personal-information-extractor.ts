062ceb7c067ca1a59e592ec06698a410d52bc18c
Andres <adsuarezq@gmail.com>
2025-11-06 17:12:57 -0500

    create directory personal-information with personal-information-extractor to move logic from extractor to this and separate code.

---
+++ src/files/personal-information/personal-information-extractor.ts
@@ -0,0 +1,38 @@
+import { getPersonalInformation } from "@/files/utils/get-personal-information.ts";
+import type { PersonalInformation } from "@/files/consts.ts";
+
+export function personalInformationExtractor({
+  pdfParsed,
+  summaryIndex,
+  languagesSkillIndex,
+  skillIndex,
+  experienceIndex,
+}: {
+  pdfParsed: string;
+  summaryIndex: number;
+  languagesSkillIndex: number;
+  skillIndex: number;
+  experienceIndex: number;
+}) {
+  let personalInformation: PersonalInformation;
+
+  const hasSummary = summaryIndex >= 0 ? true : false;
+  const indexToStartCreationOfPersonalInformation =
+    languagesSkillIndex >= 0 ? languagesSkillIndex : skillIndex;
+
+  if (hasSummary) {
+    personalInformation = getPersonalInformation({
+      startIndex: indexToStartCreationOfPersonalInformation,
+      endIndex: summaryIndex,
+      pdfParsed,
+    });
+  } else {
+    personalInformation = personalInformation = getPersonalInformation({
+      startIndex: indexToStartCreationOfPersonalInformation,
+      endIndex: experienceIndex,
+      pdfParsed,
+    });
+  }
+
+  return personalInformation;
+}
