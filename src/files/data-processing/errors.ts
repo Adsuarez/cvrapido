600316004383d3f44fb03ab4e81fda17fb15550a
Andres <adsuarezq@gmail.com>
2025-11-07 11:36:27 -0500

    move error message const from consts file to new directory data-processing and errors new file

---
+++ src/files/data-processing/errors.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = {
+  NOT_IS_LINKEDIN:
+    "The document provided is not compatible with a LinkedIn resume PDF",
+  MISSING_INFORMATION:
+    "Some required information is missing in the document, please be sure to fill your Name, Profession, Location, Languages, summary and experience",
+};
