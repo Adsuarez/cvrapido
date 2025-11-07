6cc5b95f746e3d8a910a7195f35ff2de01912316
Andres <adsuarezq@gmail.com>
2025-11-07 11:32:22 -0500

    move email regexp const to new file email inside contact directory to separate code

--- src/files/consts.ts
+++ src/files/consts.ts
@@ -31,6 +31,4 @@ const ERROR_MESSAGE = {
     "Some required information is missing in the document, please be sure to fill your Name, Profession, Location, Languages, summary and experience",
 };
 
-const EMAIL_REGEXP = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/im);
-
-export { EMAIL_REGEXP, ERROR_MESSAGE };
+export { ERROR_MESSAGE };
