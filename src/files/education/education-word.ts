7a2c6fccd4ea9d7d28f848d1892db85eb5d29d7d
Andres <adsuarezq@gmail.com>
2025-11-11 21:45:18 -0500

    create directory education and education-word file with education word class

---
+++ src/files/education/education-word.ts
@@ -0,0 +1,10 @@
+import { Word } from "@/files/words/classes.ts";
+import { Languages, Words } from "@/files/words/dictionaries.ts";
+
+class EducationWord extends Word {
+  constructor({ language }: { language: Languages }) {
+    super({ language, word: Words.Education });
+  }
+}
+
+export { EducationWord };
