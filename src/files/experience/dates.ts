8fc6f1dcf75603ccae27abde51d9e17b6cb53770
Andres <adsuarezq@gmail.com>
2025-11-11 21:45:42 -0500

    create dates object with months of the year in spanish and english

---
+++ src/files/experience/dates.ts
@@ -0,0 +1,32 @@
+import { Languages } from "../words/dictionaries";
+
+export const DATES = {
+  [Languages.Spanish]: [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre",
+  ],
+  [Languages.English]: [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ],
+};
